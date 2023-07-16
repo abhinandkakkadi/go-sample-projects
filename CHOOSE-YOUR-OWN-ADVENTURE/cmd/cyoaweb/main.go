522c863dedeea945db078566efae6f317a147019
Abhinand KR <nanduttanvsabhi@gmail.com>
2023-07-16 08:04:10 +0530

    parsed template and rendered it on screen

--- CHOOSE-YOUR-OWN-ADVENTURE/cmd/cyoaweb/main.go
+++ CHOOSE-YOUR-OWN-ADVENTURE/cmd/cyoaweb/main.go
@@ -4,13 +4,16 @@ import (
 	"choose/cyoa"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
+
+	port := flag.Int("port", 3000, "the port to start the CYOA web application on")
 	file := flag.String("file", "gopher.json", "the JSON file with CYOA story")
 	flag.Parse()
-	fmt.Printf("Using the story in %s.\n", *file)
 
 	f, err := os.Open(*file)
 	if err != nil {
@@ -22,5 +25,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server at: %d\n",*port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d",*port), h))
 }
--- CHOOSE-YOUR-OWN-ADVENTURE/cyoa/story.go
+++ CHOOSE-YOUR-OWN-ADVENTURE/cyoa/story.go
@@ -3,7 +3,53 @@ package cyoa
 import (
 	"encoding/json"
 	"io"
+	"net/http"
+	"text/template"
 )
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
+}
+
+// back ticks helps using in using multi line comments
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>{{.Title}}</h1>
+    {{range .Paragraphs}}
+    <p>{{.}}</p>
+    {{end}}
+    <ul>
+      {{range .Options}}
+      <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+      {{end}}
+    </ul>
+  </body>
+</html>
+`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
 
 func JsonStroy(r io.Reader) (Story, error) {
 	d := json.NewDecoder(r)
